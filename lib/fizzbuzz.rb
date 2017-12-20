c1ec46c60bd6cafb9a7498c8b2b6019d34308452
Jennifer <jennifer.elaine.mairead@gmail.com>
2017-12-20 13:49:25 +0000

    all tests pass

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,13 @@
 def fizzbuzz(number)
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number % 5 == 0
-  return 'fizzbuzz' if number%5 == 0 && number%3 ==0
-  number
+  if number % 3 == 0
+    if number % 5 == 0
+      return 'fizzbuzz'
+    else
+      return 'fizz'
+    end
+  elsif number%5 == 0
+    return 'buzz'
+  else
+    return number
+  end
 end
